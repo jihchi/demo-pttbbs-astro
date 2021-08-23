ddd21a682c5d91bf5a3bbe16cac0bd3ea41f814f
jihchi <achi@987.tw>
2021-08-23 21:16:00 +0800

    Pre-render 100 articles of each popluar board

--- src/shared/endpoints.mjs
+++ src/shared/endpoints.mjs
@@ -1,34 +1,26 @@
 // API document: https://api.devptt.site:5000
 const backend = 'https://api.devptt.site:3457';
 
-export const getPopularArticles = (options) => {
-  const { limit = 10, desc = "true" } = options ?? {};
-  return fetch(
-    `${backend}/api/articles/popular?limit=${limit}&desc=${desc}`
-  )
+export const getPopularArticles = (options = {}) => {
+  const { limit = 10, desc = 'true' } = options;
+  return fetch(`${backend}/api/articles/popular?limit=${limit}&desc=${desc}`);
 };
 
 export const getPopularBoards = () => {
-  return fetch(
-    `${backend}/api/boards/popular`
-  )
+  return fetch(`${backend}/api/boards/popular`);
 };
 
-export const getArticlesByBoard = (boardId, options) => {
-  const { limit = 10, desc = "true" } = options;
+export const getArticlesByBoard = (boardId, options = {}) => {
+  const { limit = 10, desc = 'true' } = options;
   return fetch(
     `${backend}/api/board/${boardId}/articles?limit=${limit}&desc=${desc}`
-  )
-}
+  );
+};
 
 export const getArticle = (articleId, boardId) => {
-  return fetch(
-    `${backend}/api/board/${boardId}/article/${articleId}`
-  )
-}
+  return fetch(`${backend}/api/board/${boardId}/article/${articleId}`);
+};
 
 export const getComments = (articleId, boardId) => {
-  return fetch(
-    `${backend}/api/board/${boardId}/article/${articleId}/comments`
-  )
-}
+  return fetch(`${backend}/api/board/${boardId}/article/${articleId}/comments`);
+};
